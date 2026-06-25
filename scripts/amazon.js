c7bd3eb75d0bad130282542d32e9f23d3133a1b0
Devansh <dubey.devansh.dev@gmail.com>
2026-06-25 08:41:23 +0530

    Making the cart-quantity Interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,8 +79,16 @@
                     productId:productId,
                     quantity:1
                     });
-                } 
+                }
 
-                console.log(cart);
+                let cartQuantity=0;
+
+                cart.forEach((item) =>{
+                    cartQuantity+=item.quantity;
+                });
+
+                document.querySelector('.js-cart-quantity')
+                .innerHTML=cartQuantity;
+                
             })
         });
