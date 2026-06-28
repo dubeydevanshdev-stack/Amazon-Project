724bee7dfd4dcb70ec1eb00ad5749677daf5cf26
Devansh <dubey.devansh.dev@gmail.com>
2026-06-29 04:23:34 +0530

    Creating Class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import { loadFromStorage } from '../data/cart.js';
 import {renderOrderSummary} from './checkout/ordersummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import '../data/cart-oop.js';
+//import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
