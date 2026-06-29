35b90103229adf3ef62cca8bf1d541f0fde8ad0c
Devansh <dubey.devansh.dev@gmail.com>
2026-06-29 19:00:58 +0530

    Adding backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import {renderOrderSummary} from './checkout/ordersummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 //import '../data/cart-oop.js';
 import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
